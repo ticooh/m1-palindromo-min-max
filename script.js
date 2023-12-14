a220441602f45ddf4779168030b1f6b261b97c7c
Leo <103613540+ticooh@users.noreply.github.com>
2023-12-13 22:32:29 -0300

    script aprovado nos testes

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+    let fraseWithoutSpace = str.toLowerCase().replace(/ /g, "");
+
+    for (let i = 0; i < parseInt(fraseWithoutSpace.length / 2); i++) {
+        if (fraseWithoutSpace[i] !== fraseWithoutSpace[fraseWithoutSpace.length - 1 - i]) {
+            return false; 
+        }
+    }
+
+    return true; 
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+
+function arrayMaxMin(arr) {
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } else if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return [ min, max ];
 }
+console.log (arrayMaxMin([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]))
